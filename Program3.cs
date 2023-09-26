b55c380fcb55d70486d3a4f7860a25b660f34078
Mr_Dipak <92545685+dipakpatil5050@users.noreply.github.com>
2023-09-26 09:44:28 +0530

    Add files via upload
    first commit for C# Language

---
+++ Program3.cs
@@ -0,0 +1,17 @@
+/* C# Program to create Number math Table*/
+
+
+System.Console.WriteLine("Please enter Number for Table : ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"{num} X 1 = {num * 1}");
+System.Console.WriteLine($"{num} X 2 = {num * 2}");
+System.Console.WriteLine($"{num} X 3 = {num * 3}");
+System.Console.WriteLine($"{num} X 4 = {num * 4}");
+System.Console.WriteLine($"{num} X 5 = {num * 5}");
+System.Console.WriteLine($"{num} X 6 = {num * 6}");
+System.Console.WriteLine($"{num} X 7 = {num * 7}");
+System.Console.WriteLine($"{num} X 8 = {num * 8}");
+System.Console.WriteLine($"{num} X 9 = {num * 9}");
+System.Console.WriteLine($"{num} X 10 = {num * 10}");
